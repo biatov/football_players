33def72ab7aa5daf2d2dcf798af67750c8ecb111
Sergey Byatov <biatov.s@gmail.com>
2017-07-10 10:28:28 +0300

    players_%data%.txt

--- plays/plays/pipelines.py
+++ plays/plays/pipelines.py
@@ -4,11 +4,13 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from datetime import datetime
 
 
 class PlaysPipeline(object):
     def open_spider(self, spider):
-        self.file = open('players.txt', 'w')
+        self.date = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+        self.file = open('players_%s.txt' % self.date, 'w')
 
     def close_spider(self, spider):
         self.file.close()
